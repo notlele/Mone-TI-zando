0a735505d14ee794c8976937412cd5185856f043
unknown <bruno.donatelli@etec.sp.gov.br>
2019-04-03 17:41:16 -0300

    [HFX] Teste2

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/index', indexRouter);
 app.use('/login', loginRouter);
 app.use('/cadastro', signupRouter);
