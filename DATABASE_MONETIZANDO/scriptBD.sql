f5bd14ccfacd58f88d0d40ae1c4cc10035a8ef88
Aluno <Aluno@Digital.net>
2019-04-24 19:58:16 -0300

    exclusão do cadastro.js

--- DATABASE_MONETIZANDO/scriptBD.sql
+++ DATABASE_MONETIZANDO/scriptBD.sql
@@ -5,7 +5,8 @@ create table TBD_BANCO(
     telefone char(15),
     email varchar(30),
     tipoContrato varchar(50),
-    qtdeAgencia char(6));
+    qtdeAgencia char(6)
+    senha varchar(30));
     
 create table TBR_AGENCIA(
 	idAgencia int primary key,
